b21e4b3348354144aaed8fda356b0f6d9b85473c
unknown <>
2017-03-01 21:47:26 +0800

    change zhushi

--- gbdt.scala
+++ gbdt.scala
@@ -1,57 +1,57 @@
 GradientBoostedTrees private def boost(  
-    input: RDD[LabeledPoint],  
-    boostingStrategy: BoostingStrategy): GradientBoostedTreesModel = {  
-  
-    val timer = new TimeTracker()  
-    timer.start("total")  
-    timer.start("init")  
-  
-    boostingStrategy.assertValid()  
-  
+	input: RDD[LabeledPoint],  
+	boostingStrategy: BoostingStrategy): GradientBoostedTreesModel = {  
+
+	val timer = new TimeTracker()  
+	timer.start("total")  
+	timer.start("init")  
+
+	boostingStrategy.assertValid()  
+
 	// 初始化GBDT参数
 	//迭代次数  
 	val numIterations = boostingStrategy.numIterations  
 	//每次迭代的决策树模型  
-    val baseLearners = new Array[DecisionTreeModel](numIterations)  
+	val baseLearners = new Array[DecisionTreeModel](numIterations)  
 	//每个树模型的权重   
 	val baseLearnerWeights = new Array[Double](numIterations)  
 	//损失的计算方式，参考下表  
 	val loss = boostingStrategy.loss  
 	//学习率
 	val learningRate = boostingStrategy.learningRate  
-    // Prepare strategy for individual trees, which use regression with variance impurity.  
+	// Prepare strategy for individual trees, which use regression with variance impurity.  
 	val treeStrategy = boostingStrategy.treeStrategy.copy  
 	//algo支持c分类和回归，此处使用回归
-    treeStrategy.algo = Regression  
-    treeStrategy.impurity = Variance  
-    treeStrategy.assertValid()  
-  
-    // Cache input,缓存样本集合  
-    if (input.getStorageLevel == StorageLevel.NONE) {  
-      input.persist(StorageLevel.MEMORY_AND_DISK)  
-    }  
-  
-    timer.stop("init")  
-  
-    logDebug("##########")  
-    logDebug("Building tree 0")  
-    logDebug("##########")  
-    var data = input  
-  
-    //初始化树
-    timer.start("building tree 0")  
-    val firstTreeModel = new DecisionTree(treeStrategy).run(data)  
-    baseLearners(0) = firstTreeModel  
-    baseLearnerWeights(0) = 1.0  
-    val startingModel = new GradientBoostedTreesModel(Regression, Array(firstTreeModel), Array(1.0))  
-    logDebug("error of gbt = " + loss.computeError(startingModel, input))  
-    timer.stop("building tree 0")  
-  
-    data = input.map(point => LabeledPoint(loss.gradient(startingModel, point),  point.features))  
-  
+	treeStrategy.algo = Regression  
+	treeStrategy.impurity = Variance  
+	treeStrategy.assertValid()  
+
+	// Cache input,缓存样本集合  
+	if (input.getStorageLevel == StorageLevel.NONE) {  
+	  input.persist(StorageLevel.MEMORY_AND_DISK)  
+	}  
+
+	timer.stop("init")  
+
+	logDebug("##########")  
+	logDebug("Building tree 0")  
+	logDebug("##########")  
+	var data = input  
+
+	//初始化树
+	timer.start("building tree 0")  
+	val firstTreeModel = new DecisionTree(treeStrategy).run(data)  
+	baseLearners(0) = firstTreeModel  
+	baseLearnerWeights(0) = 1.0  
+	val startingModel = new GradientBoostedTreesModel(Regression, Array(firstTreeModel), Array(1.0))  
+	logDebug("error of gbt = " + loss.computeError(startingModel, input))  
+	timer.stop("building tree 0")  
+
+	data = input.map(point => LabeledPoint(loss.gradient(startingModel, point),  point.features))  
+
 	var m = 1  
 	//迭代训练各树模型  
-    while (m < numIterations) {  
+	while (m < numIterations) {  
 		timer.start(s"building tree $m")  
 		logDebug("###################################################")  
 		logDebug("Gradient boosting tree iteration " + m)  
@@ -71,10 +71,10 @@ GradientBoostedTrees private def boost(
 		m += 1  
     }  
   
-    timer.stop("total")  
-  
-    logInfo("Internal timing for DecisionTree:")  
-    logInfo(s"$timer")  
+	timer.stop("total")  
+
+	logInfo("Internal timing for DecisionTree:")  
+	logInfo(s"$timer")  
 	//最后融合各颗树的模型  
-    new GradientBoostedTreesModel(boostingStrategy.treeStrategy.algo, baseLearners, baseLearnerWeights)  
+	new GradientBoostedTreesModel(boostingStrategy.treeStrategy.algo, baseLearners, baseLearnerWeights)  
   }  
